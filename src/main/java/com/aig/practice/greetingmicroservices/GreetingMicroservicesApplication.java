2130fb93d315e79bafa858f50cebccc7fe2f6c70
weakon123-stack <54553241+weakon123-stack@users.noreply.github.com>
2019-08-26 15:15:49 -0500

    Initial Commit

---
+++ src/main/java/com/aig/practice/greetingmicroservices/GreetingMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.aig.practice.greetingmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingMicroservicesApplication.class, args);
+	}
+
+}
